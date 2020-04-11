96dcc5928782c15e9cfd8c1825f36d081f83c180
gabriel <gortega2001@gmail.com>
2020-04-11 15:06:36 -0500

    g

--- FisicaFInal/src/vista/VPrincipal.java
+++ FisicaFInal/src/vista/VPrincipal.java
@@ -29,7 +29,6 @@ public class VPrincipal extends JFrame implements MouseMotionListener, MouseList
 	private PanelPlano pPlano;
 	private VMagnitud vMagnitud;
 	private VPosicion vPosicion;
-	private VBienvenido vbien;
 	private int contP = 0, contN = 0;
 	private ArrayList<JButton> btnPositivos;
 	private ArrayList<JButton> btnNegativos;

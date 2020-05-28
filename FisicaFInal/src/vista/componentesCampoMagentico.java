73212c7809b549f041f95f1aa64f6b715645f32d
gabriel <gortega2001@gmail.com>
2020-05-27 19:41:10 -0500

    ff

--- FisicaFInal/src/vista/componentesCampoMagentico.java
+++ FisicaFInal/src/vista/componentesCampoMagentico.java
@@ -1,16 +1,21 @@
 package vista;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeListener;
 
-public class componentesCampoMagentico {
+public class componentesCampoMagentico implements ActionListener {
 	
 	protected JButton calcular;
-	protected SpinnerModel modeloX, modeloY;
+	private SpinnerModel modeloX, modeloY;
 	protected JLabel xLblVel, yLblVel;
 	protected JSpinner xVelocidad, yVelocidad;
 	protected JLabel xLblCampo, yLblCampo;
@@ -24,10 +29,69 @@ public class componentesCampoMagentico {
 	
 	public componentesCampoMagentico(VPrincipal vPrincipal, int margenX) {
 		this.vPrincipal = vPrincipal;
-		
-		
-	}
-	
-	
+		calcular = new JButton("Calcular");
+		xLblVel = new JLabel();
+		yLblVel = new JLabel();
+		xVelocidad = new JSpinner();
+		yVelocidad = new JSpinner();
+		xLblCampo = new JLabel();
+		yLblCampo = new JLabel();
+		xCampoMagnetico = new JSpinner();
+		yCampoMagnetico = new JSpinner();
+		lblCarga = new JLabel();
+		lblInfo = new JLabel();
+		lblPorDiez = new JLabel();
+		lblcoulomb = new JLabel();
+		btnAceptar = new JButton("Aceptar");
+		notacion = new JComboBox<Integer>();
+		magnitud = new JTextArea();
+		modeloY = new SpinnerNumberModel();
+		modeloX = new SpinnerNumberModel();
 
+	}
+	public void invisible() {
+		calcular.setVisible(false);
+		xLblVel.setVisible(false);
+		yLblVel.setVisible(false);
+		xVelocidad.setVisible(false);
+		yVelocidad.setVisible(false);
+		xLblCampo.setVisible(false);
+		yLblCampo.setVisible(false);
+		xCampoMagnetico.setVisible(false);
+		yCampoMagnetico.setVisible(false);
+		lblCarga.setVisible(false);
+		lblInfo.setVisible(false);
+		lblPorDiez.setVisible(false);
+		lblcoulomb.setVisible(false);
+		btnAceptar.setVisible(false);
+		notacion.setVisible(false);
+		magnitud.setVisible(false); 
+	}
+	public void visible() {
+		calcular.setVisible(true);
+		calcular.addActionListener(vPrincipal);
+		xLblVel.setVisible(true);
+		yLblVel.setVisible(true);
+		xVelocidad.setVisible(true);
+		yVelocidad.setVisible(true);
+		xLblCampo.setVisible(true);
+		yLblCampo.setVisible(true);
+		xCampoMagnetico.setVisible(true);
+		yCampoMagnetico.setVisible(true);
+		lblCarga.setVisible(true);
+		lblInfo.setVisible(true);
+		lblPorDiez.setVisible(true);
+		lblcoulomb.setVisible(true);
+		btnAceptar.setVisible(true);
+		notacion.setVisible(true);
+		magnitud.setVisible(true); 
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String ac = e.getActionCommand();
+		if(ac.equals("Calcular")) {
+			
+		}
+	}
 }
